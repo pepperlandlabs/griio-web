808a2cec15440b1a1eac53a7831ff792a15a0f2d
Edmund Salvacion <edmund@edmundatwork.com>
2012-04-08 18:17:51 -0700

    [CHORE] Adds basic auth to non-dev envs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  protected
+  def authenticate
+    unless Rails.env.development?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "griio" && password == "lo90p;"
+      end
+    end
+  end
 end
