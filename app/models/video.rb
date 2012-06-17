83909e43bcddc628598e7897d6a0a4e449a230ba
Edmund Salvacion <edmund@edmundatwork.com>
2012-06-17 15:14:43 -0700

    [FEATURE] Creates initial video show page

--- app/models/video.rb
+++ app/models/video.rb
@@ -12,4 +12,18 @@ class Video
   index({ href: 1 }, { unique: true })
 
   validates_uniqueness_of :href
+
+  def embed_url
+    case source
+    when :youtube
+      "http://www.youtube.com/embed/#{self.source_id}?autoplay=1&modestbranding=1&amp;showinfo=0&amp;wmode=opaque"
+    end
+  end
+
+  def source_id
+    case source
+    when :youtube
+      self.href.scan(/v=([\w-]+)&*/).flatten.first
+    end
+  end
 end
